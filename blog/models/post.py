638499b25d00841573b683915a8e31b4bec1fa31
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-11-30 14:17:19 +0100

    Added blog posts model

---
+++ blog/models/post.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Post model
+"""
+
+from .base_model import BaseModel, models
+from .user import User
+
+
+class BlogPosts(BaseModel):
+    """
+    Post model class
+    columns:
+    - title: title of the post
+    content: content of the post
+    sub_title: subtitle of the post
+    author: author of the post
+    """
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    sub_title = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'blog_posts'
+
+    def __str__(self) -> str:
+        super().__str__()
+        return f"Post {self.id} {self.title}"
